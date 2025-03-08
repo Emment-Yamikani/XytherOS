ef0e794ebd5406342578a1f61d3d27db5f0639c8
Emment-Yamikani <emmentyamikani@gmail.com>
2025-03-08 20:29:54 +0200

    refactored sigmask()

--- kernel/sys/signal/sigmask.c
+++ kernel/sys/signal/sigmask.c
@@ -2,42 +2,35 @@
 #include <sys/_signal.h>
 #include <sys/thread.h>
 
-static void do_mask(sigset_t *sigset, const sigset_t *set) {
-    for (usize i = 0; i < __NR_INT; ++i) {
-        sigset->sigset[i] &= ~set->sigset[i];
-    }
-}
-
-static void do_set(sigset_t *sigset, const sigset_t *set) {
-    for (usize i = 0; i < __NR_INT; ++i) {
-        sigset->sigset[i] |= set->sigset[i];
-    }
+static int do_sigmask(sigset_t *sigset, int how, const sigset_t *set) {
+    if (how == SIG_SETMASK) {
+        *sigset = *set;
+    } else if (how == SIG_BLOCK) {
+        for (usize i = 0; i < __NR_INT; ++i) {
+            sigset->sigset[i] |= set->sigset[i];
+        }
+    } else if (how == SIG_UNBLOCK) {
+        for (usize i = 0; i < __NR_INT; ++i) {
+            sigset->sigset[i] &= ~set->sigset[i];
+        }
+    } else return -EINVAL;
+    
+    return 0;
 }
 
 int sigmask(sigset_t *sigset, int how, const sigset_t *set, sigset_t *oset) {
-    if (!sigset) return -EINVAL;  // Early return
+    if (!sigset) return -EINVAL;
 
     if (oset) *oset = *sigset;
 
-    if (!set) return 0;  // Early return
+    if (!set) return 0;
 
-    if (sigismember(set, SIGKILL) || sigismember(set, SIGSTOP))
-        return -EINVAL;  // Restriction check
-
-    if (how == SIG_SETMASK) {
-        *sigset = *set;
-        return 0;
-    } else if (how == SIG_BLOCK) {
-        do_set(sigset, set);
-        return 0;
-    } else if (how == SIG_UNBLOCK) {
-        do_mask(sigset, set);
-        return 0;
+    if (sigismember(set, SIGKILL) || sigismember(set, SIGSTOP)) {
+        return -EINVAL;
     }
-    
-    return -EINVAL;
-}
 
+    return do_sigmask(sigset, how, set);
+}
 
 int thread_sigmask(thread_t *thread, int how, const sigset_t *set, sigset_t *oset) {
     if (thread == NULL)
