4ce7d19321e1b5fd6298c3747e0eff04a9ac3732
Emment Yamikani Banda <emmentyamikani@gmail.com>
2025-04-15 22:36:11 +0200

    Enhanced CGA escape sequence processing.

--- kernel/include/dev/cga.h
+++ kernel/include/dev/cga.h
@@ -4,23 +4,49 @@
 
 extern int use_cga;
 
-#define CGA_BLACK           0
-#define CGA_BLUE            1
-#define CGA_GREEN           2
-#define CGA_CYAN            3
-#define CGA_RED             4
-#define CGA_MAGENTA         5
-#define CGA_BROWN           6
-#define CGA_LIGHT_GREY      7
-#define CGA_DARK_GREY       8
-#define CGA_LIGHT_BLUE      9
-#define CGA_LIGHT_GREEN     10
-#define CGA_LIGHT_CYAN      11
-#define CGA_LIGHT_RED       12
-#define CGA_LIGHT_MAGENTA   13
-#define CGA_YELLOW          14
-#define CGA_WHITE           15
+// CGA color codes
+enum cga_colors {
+    CGA_BLACK   = 0,
+    CGA_BLUE    = 1,
+    CGA_GREEN   = 2,
+    CGA_CYAN    = 3,
+    CGA_RED     = 4,
+    CGA_MAGENTA = 5,
+    CGA_BROWN   = 6,
+    CGA_LGREY   = 7,
+    CGA_DGREY   = 8,
+    CGA_LBLUE   = 9,
+    CGA_LGREEN  = 10,
+    CGA_LCYAN   = 11,
+    CGA_LRED    = 12,
+    CGA_LMAGENTA= 13,
+    CGA_YELLOW  = 14,
+    CGA_WHITE   = 15
+};
 
-int cga_init(void);
-void cga_putc(const int);
-size_t cga_puts(const char *s);
+#define CGA_RESET       "\e[0m"
+
+#define CGA_FG_BLACK    "\e[30m"
+#define CGA_FG_BLUE     "\e[31m"
+#define CGA_FG_GREEN    "\e[32m"
+#define CGA_FG_CYAN     "\e[33m"
+#define CGA_FG_RED      "\e[34m"
+#define CGA_FG_MAGENTA  "\e[35m"
+#define CGA_FG_BROWN    "\e[36m"
+#define CGA_FG_LGREY    "\e[37m"
+
+#define CGA_BG_BLACK    "\e[40m"
+#define CGA_BG_BLUE     "\e[41m"
+#define CGA_BG_GREEN    "\e[42m"
+#define CGA_BG_CYAN     "\e[43m"
+#define CGA_BG_RED      "\e[44m"
+#define CGA_BG_MAGENTA  "\e[45m"
+#define CGA_BG_BROWN    "\e[46m"
+#define CGA_BG_LGREY    "\e[47m"
+
+extern void     cga_putC(char c);
+extern int      cga_init(void);
+extern void     cga_putc(const int);
+extern size_t   cga_puts(const char *s);
+extern void     cga_putchar_at(const int c, int x, int y);
+extern void     cga_puts_at(const char *buf, int x, int y);
